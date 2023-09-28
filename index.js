5813e717a0e6229e0c55c9866253c21d8d7cb707
Ray Sakala <103606571+RayTheGreat18@users.noreply.github.com>
2023-09-28 14:46:33 +0200

    add css

--- index.js
+++ index.js
@@ -2,45 +2,48 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 const passwordBtn = document.getElementById("password-btn")
-let outPut1 = document.getElementById("out-put1")
-let outPut2 = document.getElementById("out-put2")
+const outPut1 = document.getElementById("out-put1")
+const outPut2 = document.getElementById("out-put2")
+const copyToClipboard1 = document.getElementById("copy-to-clipboard1")
+const copyToClipboard2 = document.getElementById("copy-to-clipboard2")
 
 
 passwordBtn.addEventListener("click", function() {
     
-    let randomIndex1 = Math.floor( Math.random() * characters.length )
-    let randomIndex2 = Math.floor( Math.random() * characters.length )
-    let randomIndex3 = Math.floor( Math.random() * characters.length )
-    let randomIndex4 = Math.floor( Math.random() * characters.length )
-    let randomIndex5 = Math.floor( Math.random() * characters.length )
-    let randomIndex6 = Math.floor( Math.random() * characters.length )
-    let randomIndex7 = Math.floor( Math.random() * characters.length )
-    let randomIndex8 = Math.floor( Math.random() * characters.length )
-    let randomIndex9 = Math.floor( Math.random() * characters.length )
-    let randomIndex10 = Math.floor( Math.random() * characters.length )
-    let randomIndex11 = Math.floor( Math.random() * characters.length )
-    let randomIndex12 = Math.floor( Math.random() * characters.length )
-    let randomIndex13 = Math.floor( Math.random() * characters.length )
-    let randomIndex14 = Math.floor( Math.random() * characters.length )
-    let randomIndex15 = Math.floor( Math.random() * characters.length )
+    // Password generator for each later for the first output
+    const randomIndex1 = Math.floor( Math.random() * characters.length )
+    const randomIndex2 = Math.floor( Math.random() * characters.length )
+    const randomIndex3 = Math.floor( Math.random() * characters.length )
+    const randomIndex4 = Math.floor( Math.random() * characters.length )
+    const randomIndex5 = Math.floor( Math.random() * characters.length )
+    const randomIndex6 = Math.floor( Math.random() * characters.length )
+    const randomIndex7 = Math.floor( Math.random() * characters.length )
+    const randomIndex8 = Math.floor( Math.random() * characters.length )
+    const randomIndex9 = Math.floor( Math.random() * characters.length )
+    const randomIndex10 = Math.floor( Math.random() * characters.length )
+    const randomIndex11 = Math.floor( Math.random() * characters.length )
+    const randomIndex12 = Math.floor( Math.random() * characters.length )
+    const randomIndex13 = Math.floor( Math.random() * characters.length )
+    const randomIndex14 = Math.floor( Math.random() * characters.length )
+    const randomIndex15 = Math.floor( Math.random() * characters.length )
     
     
-    
-    let randomIndex1a = Math.floor( Math.random() * characters.length )
-    let randomIndex2a = Math.floor( Math.random() * characters.length )
-    let randomIndex3a = Math.floor( Math.random() * characters.length )
-    let randomIndex4a = Math.floor( Math.random() * characters.length )
-    let randomIndex5a = Math.floor( Math.random() * characters.length )
-    let randomIndex6a = Math.floor( Math.random() * characters.length )
-    let randomIndex7a = Math.floor( Math.random() * characters.length )
-    let randomIndex8a = Math.floor( Math.random() * characters.length )
-    let randomIndex9a = Math.floor( Math.random() * characters.length )
-    let randomIndex10a = Math.floor( Math.random() * characters.length )
-    let randomIndex11a = Math.floor( Math.random() * characters.length )
-    let randomIndex12a = Math.floor( Math.random() * characters.length )
-    let randomIndex13a = Math.floor( Math.random() * characters.length )
-    let randomIndex14a = Math.floor( Math.random() * characters.length )
-    let randomIndex15a = Math.floor( Math.random() * characters.length )
+    // Password generator for each later for the second output
+    const randomIndex1a = Math.floor( Math.random() * characters.length )
+    const randomIndex2a = Math.floor( Math.random() * characters.length )
+    const randomIndex3a = Math.floor( Math.random() * characters.length )
+    const randomIndex4a = Math.floor( Math.random() * characters.length )
+    const randomIndex5a = Math.floor( Math.random() * characters.length )
+    const randomIndex6a = Math.floor( Math.random() * characters.length )
+    const randomIndex7a = Math.floor( Math.random() * characters.length )
+    const randomIndex8a = Math.floor( Math.random() * characters.length )
+    const randomIndex9a = Math.floor( Math.random() * characters.length )
+    const randomIndex10a = Math.floor( Math.random() * characters.length )
+    const randomIndex11a = Math.floor( Math.random() * characters.length )
+    const randomIndex12a = Math.floor( Math.random() * characters.length )
+    const randomIndex13a = Math.floor( Math.random() * characters.length )
+    const randomIndex14a = Math.floor( Math.random() * characters.length )
+    const randomIndex15a = Math.floor( Math.random() * characters.length )
     
     outPut1.textContent = 
     characters[randomIndex1] + characters[randomIndex2] + characters[randomIndex3] + characters[randomIndex4] + characters[randomIndex5] + characters[randomIndex6] + characters[randomIndex7] + characters[randomIndex8] + characters[randomIndex9] + characters[randomIndex10] + characters[randomIndex11] + characters[randomIndex12] + characters[randomIndex13] + characters[randomIndex14] + characters[randomIndex15]
@@ -51,6 +54,14 @@ passwordBtn.addEventListener("click", function() {
     characters[randomIndex1a] + characters[randomIndex2a] + characters[randomIndex3a] + characters[randomIndex4a] + characters[randomIndex5a] + characters[randomIndex6a] + characters[randomIndex7a] + characters[randomIndex8a] + characters[randomIndex9a] + characters[randomIndex10a] + characters[randomIndex11a] + characters[randomIndex12a] + characters[randomIndex13a] + characters[randomIndex14a] + characters[randomIndex15a]
     
     
+})
+
+copyToClipboard1.addEventListener("click", () => {
+    console.log("Copy 1 works")
+})
+
+copyToClipboard2.addEventListener("click", () => {
+    console.log("Copy 2 works")
 })
 
 
